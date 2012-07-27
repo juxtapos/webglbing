806e6667723a613dfc95fa984d14ce9b8a1dc014
juxtapos <claus@formatvorlage.de>
2012-07-27 09:14:54 +0200

    added selection code

--- web/js/webglbing.js
+++ web/js/webglbing.js
@@ -1,16 +1,23 @@
 var lat = 49.2936;
 var long = 8.6418;
-var zoom = 12;
+var zoom = 8;
 var neighbours = 3;
 
 // duisburg
 var lat = 51.4314308166504;
 var long = 6.76392984390259;
 
+// Germany
+var lat = '51.2019996643066';
+var long = '10.3819999694824';
+
 var camerapos = new CameraPos(),
     scene, 
     camera, 
-    renderer;
+    renderer, 
+    ground;
+var projector = new THREE.Projector();
+var mouse = { x: 0, y: 0 }, INTERSECTED;
 
 window.addEventListener('load', init, false)
 
@@ -18,6 +25,21 @@ function init () {
     createGui();    
     loadScene();
     animate();
+
+    document.addEventListener('mousedown', function (event) {
+        onDocumentMouseDown(event);
+    }, false);
+
+    document.addEventListener('mouseup', function (event) {
+        console.log(INTERSECTED);
+    }, false);
+
+    function onDocumentMouseDown( event ) {
+        event.preventDefault();
+
+        mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+        mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    }
 }
 
 function matrix(lat, long, zoom, neighbours) {
@@ -58,17 +80,19 @@ function createGui () {
     c.onChange(cam);
     var c = gui.add(text, 'z', -500, 500).step(1);
     c.onChange(cam);
-    var c = gui.add(text, 'zoom', 1, 25).step(1);
-    c.onChange(cam);
+    var c = gui.add(text, 'zoom', 1, 20).step(1);
+    c.onChange(function (val) {
+        zoom = val;
+        scene.remove(ground);
+        ground = createGround();
+        scene.add(ground)
+    });
 
     var lastZoom = zoom;
 
     function cam() {
         camera.position.set(text.x, text.y, text.z);
-        camera.lookAt(scene.position);   
-        if (lastZoom != camerapos.zoom) {
-
-        }
+        camera.lookAt(scene.position);
     }
 }    
 
@@ -109,17 +133,17 @@ function loadScene() {
     scene.add(pointLight); 
 
     var dl = new THREE.DirectionalLight(0xff0000);
-    dl.position.set(210, 200, 20).normalize();
+    dl.position.set(210, 200, 200).normalize();
     dl.intensity = 10;
     dl.castShadow = true;
     scene.add(dl);
 
-    // spotlight #1 -- yellow, dark shadow
-    // var spotlight = new THREE.SpotLight(0xffff00);
-    // sotlight.position.set(-60,550,-30);
+    //spotlight #1 -- yellow, dark shadow
+    // var spotlight = new THREE.SpotLight(0xff0000);
+    // spotlight.position.set(-60,550,-30);
     // //spotlight.shadowCameraVisible = true;
     // spotlight.shadowDarkness = 0.95;
-    // spotlight.intensity = 0.1;
+    // spotlight.intensity = 10;
     // // must enable shadow casting ability for the light
     // spotlight.castShadow = true;
     // scene.add(spotlight);
@@ -143,20 +167,45 @@ function loadScene() {
     // using THREE.MeshFaceMaterial() in the constructor below
     //   causes the mesh to use the materials stored in the geometry
     cube = new THREE.Mesh( cubeGeometry, new THREE.MeshFaceMaterial() );
-    cube.position.set(400, 0, 180);
+    cube.position.set(000, 0, 80);
     cube.castShadow = true;
     scene.add( cube );
 
-    var m = matrix(lat, long, zoom, neighbours);
 
+
+    var geometry = new THREE.CylinderGeometry( 10, 0.1, 100, 64 );
+    //geometry.applyMatrix( new THREE.Matrix4().setRotationFromEuler( new THREE.Vector3( Math.PI / 2, Math.PI, 0 ) ) );
+    var material = new THREE.MeshBasicMaterial( { color: 0x3333DD } );
+
+    for ( var i = 0; i < 20; i ++ ) {
+
+        var mesh = new THREE.Mesh( geometry, material );
+        mesh.position.x = Math.random() * 4000 - 2000;
+        mesh.position.y = 40;
+        mesh.position.z = Math.random() * 4000 - 2000;
+        //mesh.scale.x = mesh.scale.y = mesh.scale.z = Math.random() * 4 + 2;
+        mesh.lookAt(0, 0, 0);
+        mesh.castShadow = true;
+        scene.add( mesh );
+
+    }
+
+
+
+    ground = createGround();
+
+    scene.add(ground);
+}
+
+function createGround () {
+    console.log('cd')
+    var m = matrix(lat, long, zoom, neighbours);
     var flags = 'A'; // aerial
     var flags = 'A,G'; // aerial + roads + borders
     var flags = 'A,G,L'; // aerial + roads + borders + labels
-
     var flags = 'G'; // roads + borders 
     var flags = 'G,LA';
     var flags = 'G,VE,BX,LA'; // roads + borders + labels
-
     var ground = new THREE.Object3D();
 
     for (var y = 0; y < m.length; y++) {
@@ -173,11 +222,8 @@ function loadScene() {
             YY = obj;
         }
     }
-
-    scene.add(ground);
+    return ground;
 }
-
-var YY = null;
 
 function createTile (quadkey, url) { 
     // floor: mesh to receive shadows
@@ -197,5 +243,29 @@ function animate() {
 }
 
 function render() {   
-    renderer.render( scene, camera );
+    var vector = new THREE.Vector3(mouse.x, mouse.y, 1);
+    projector.unprojectVector(vector, camera);
+
+    var ray = new THREE.Ray(camera.position, vector.subSelf( camera.position ).normalize());
+
+    var intersects = ray.intersectObjects(ground.children);
+
+    if (intersects.length > 0) {
+        if (INTERSECTED != intersects[0].object) {
+            //if ( INTERSECTED ) INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
+            INTERSECTED = intersects[0].object;
+            //INTERSECTED.currentHex = INTERSECTED.material.color.getHex();
+            //INTERSECTED.material.color.setHex( 0xff0000 );
+        }
+    } else {
+        //if ( INTERSECTED ) INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
+        INTERSECTED = null;
+    }
+
+    for (var i = 1, l = ground.children.length; i < l; i++) {
+        ground.children[i].lookAt(1, 500, 500);
+
+    }
+
+    renderer.render(scene, camera);
 }
